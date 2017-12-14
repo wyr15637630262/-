a9e10b4baef452540518193fa4a030a9d8d0bfcc
wyr16537630262 <wyr15637630262@163.com>
2017-12-14 13:43:33 +0800

    first commit

---
+++ pages/kind/kind.js
@@ -0,0 +1,113 @@
+// pages/kind/kind.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hotList: [
+      {
+        pic: '/utils/images/cake.png',
+        title: '纸杯蛋糕',
+        desc: '￥20'
+      }, {
+        pic: '/utils/images/cake.png',
+        title: '纸杯蛋糕',
+        desc: '￥20'
+      }, {
+        pic: '/utils/images/cake.png',
+        title: '纸杯蛋糕',
+        desc: '￥20'
+      }, {
+        pic: '/utils/images/cake.png',
+        title: '纸杯蛋糕',
+        desc: '￥20'
+      }, {
+        pic: '/utils/images/cake.png',
+        title: '纸杯蛋糕',
+        desc: '￥20'
+      }
+    ],
+    dataList: [
+      {
+        pic: '/utils/images/makalong.jpg',
+        title: '马卡龙',
+        desc: '￥50'
+      }, {
+        pic: '/utils/images/makalong.jpg',
+        title: '马卡龙',
+        desc: '￥50'
+      }, {
+        pic: '/utils/images/makalong.jpg',
+        title: '马卡龙',
+        desc: '￥50'
+      }, {
+        pic: '/utils/images/makalong.jpg',
+        title: '马卡龙',
+        desc: '￥50'
+      }, {
+        pic: '/utils/images/makalong.jpg',
+        title: '马卡龙',
+        desc: '￥50'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '菜单',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/our/adress/adress.js
@@ -0,0 +1,73 @@
+// pages/our/adress/adress.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '我的地址',
+    })
+  },
+  // 新增地址页面
+  toAdd:function(){
+    wx.navigateTo({
+      url: 'addAdress/addAdress',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
